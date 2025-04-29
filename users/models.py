4a20bed14c3711a1272d93f781ba96c99822a0fa
Vladislav Lokhanev <88274622+VladislavLF@users.noreply.github.com>
2025-04-29 14:43:38 +0300

    ДЗ 1-5

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import models
+from django import forms
+
+# Create your models here.
