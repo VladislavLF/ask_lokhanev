f8f96b3097d68d9b24d5f9633a4119adfdcd8428
Vladislav Lokhanev <88274622+VladislavLF@users.noreply.github.com>
2025-05-16 14:47:51 +0300

    Delete users directory

--- users/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
-from django.db import models
-from django import forms
-
-# Create your models here.
